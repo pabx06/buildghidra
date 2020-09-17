196bc5593680e4861dd8bedac2c951fd8783755c
johny english <pabx06@gmail.com>
2020-09-17 17:47:12 +0200

    fixed PATH

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH="${PWD}/downloads/amazon-corretto-11-x64-linux-jdk/bin"
+export PATH="${PWD}/downloads/amazon-corretto-11-x64-linux-jdk/bin:$PATH"
 git clone git@github.com:NationalSecurityAgency/ghidra.git
 cd ghidra
 gradle --init-script gradle/support/fetchDependencies.gradle init 
