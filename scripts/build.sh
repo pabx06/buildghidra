a834928b76b2580b0e352725b738b4baf031afb5
johny english <pabx06@gmail.com>
2020-09-17 21:58:19 +0200

    quite downloads

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="${PWD}/downloads/amazon-corretto-11.0.8.10.1-linux-x64/bin/:$PATH"
 git clone https://github.com/NationalSecurityAgency/ghidra.gi
-tcd ghidra
+cd ghidra
 gradle -q --init-script gradle/support/fetchDependencies.gradle init >/dev/null
 gradle -q buildGhidra
