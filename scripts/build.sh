ba333723069e8015b2904ecb8842ce17b265f7a5
johny english <pabx06@gmail.com>
2020-09-18 00:17:12 +0200

    customs colors and look and feel

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,19 @@
 export PATH="${PWD}/downloads/amazon-corretto-11.0.8.10.1-linux-x64/bin/:$PATH"
 git clone https://github.com/NationalSecurityAgency/ghidra.git
 cd ghidra
+
+#patching colors
+patch ./Ghidra/Framework/Docking/src/main/java/docking/widgets/AbstractGCellRenderer.java  ../patch/AbstractGCellRenderer.patch
+patch ./Ghidra/Framework/Docking/src/main/java/docking/widgets/filter/FilterTextField.java  ../patch/FilterTextField.patch
+patch ./Ghidra/Framework/Docking/src/main/java/docking/widgets/table/GTableHeaderRenderer.java ../patch/GTableHeaderRenderer.patch
+patch ./Ghidra/Framework/Docking/src/main/java/ghidra/docking/util/DockingWindowsLookAndFeelUtils.java ../patch/DockingWindowsLookAndFeelUtils.patch
+#patch launch.sh
+patch ./Ghidra/RuntimeScripts/Common/support/launch.properties  ../patch/launch.properties.patch
+patch ./Ghidra/RuntimeScripts/Linux/support/launch.sh ../patch/launch.sh.patch
+
+#flatlaf
+patch ./Ghidra/Framework/Docking/build.gradle ../patch/build.gradle.patch
+patch ./gradle/support/fetchDependencies.gradle ../patch/fetchDependencies.gradle.patch
+
 gradle -q --init-script gradle/support/fetchDependencies.gradle init >/dev/null
 gradle -q buildGhidra
--- scripts/download.sh
+++ scripts/download.sh
@@ -3,4 +3,4 @@ mkdir -p downloads
 cd downloads
 #wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
 wget -q https://corretto.aws/downloads/resources/11.0.8.10.1/amazon-corretto-11.0.8.10.1-linux-x64.tar.gz
-tar xzvf amazon-corretto-11.0.8.10.1-linux-x64.tar.gz
+tar xzf amazon-corretto-11.0.8.10.1-linux-x64.tar.gz
