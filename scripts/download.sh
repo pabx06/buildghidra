c67b598afb9980499f58ea6720677bee4e1090b4
johny english <pabx06@gmail.com>
2020-09-17 17:52:46 +0200

    fixed PATH

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p downloads
 cd downloads
-wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
-tar xzvf amazon-corretto-11-x64-linux-jdk.tar.gz
+#wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
+wget https://corretto.aws/downloads/resources/11.0.8.10.1/amazon-corretto-11.0.8.10.1-linux-x64.tar.gz
+tar xzvf amazon-corretto-11.0.8.10.1-linux-x64.tar.gz
