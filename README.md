8c75127ec14aaf9dadc963d9dfa8cc99ae24fff1
johny english <pabx06@gmail.com>
2020-10-02 04:03:43 +0200

    ARM manual and more color changes

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 ⋅⋅* [v1.0.3](https://github.com/pabx06/buildghidra/releases/tag/v1.0.3) ghidra 0cdc722921cef61b7ca1b7236bdc21079fd4c03e
 ⋅⋅* [v1.0.2](https://github.com/pabx06/buildghidra/releases/tag/v1.0.2) ghidra 7e5f015d651b9c5e7825cb10bc50338b5af67538
 2. Patch _code_browser.tcd if not already done `~/.ghidra/.ghidra_9.2_DEV/tools/_code_browser.tcd` to `~/.ghidra/.ghidra_9.2_DEV/tools/_code_browser_dark_night.tcd`  with [ghidra_darknight](https://github.com/elliiot/ghidra_darknight)
+
+### ChangeLog
+- v1.1.0  Color About,Tips of the day, Bytes Viewer, save data,  CodeBrowser Tooltip, ARM manual
--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,14 @@ patch ./Ghidra/Framework/Docking/src/main/java/docking/widgets/table/GTableHeade
 patch ./Ghidra/Framework/Docking/src/main/java/ghidra/docking/util/DockingWindowsLookAndFeelUtils.java ../patch/DockingWindowsLookAndFeelUtils.patch
 patch ./Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/function/editor/FunctionEditorDialog.java ../patch/FunctionEditorDialog.java
 patch ./Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/function/editor/FunctionEditorDialog.java ../patch/FunctionEditorDialog.java.bis
+
+patch ./Ghidra/Framework/Project/src/main/java/ghidra/framework/main/SaveDataDialog.java ../patch/SaveDataDialog.patch
+patch ./Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/hover/AbstractReferenceHover.java  ../patch/AbstractReferenceHover.patch
+patch ./Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/totd/TipOfTheDayDialog.java  ../patch/TipOfTheDayDialog.patch
+patch ./Ghidra/Features/Base/src/main/java/ghidra/framework/main/InfoPanel.java  ../patch/InfoPanel.patch
+patch ./Ghidra/Features/ByteViewer/src/main/java/ghidra/app/plugin/core/byteviewer/ByteViewerPanel.java  ../patch/ByteViewerPanel.patch
+patch ./Ghidra/Features/ByteViewer/src/main/java/ghidra/app/plugin/core/byteviewer/ByteViewerComponent.java  ../patch/ByteViewerComponent.patch
+
 #patch launch.sh
 patch ./Ghidra/RuntimeScripts/Common/support/launch.properties  ../patch/launch.properties.patch
 patch ./Ghidra/RuntimeScripts/Linux/support/launch.sh ../patch/launch.sh.patch
@@ -22,6 +30,10 @@ patch ./Ghidra/RuntimeScripts/Linux/support/launch.sh ../patch/launch.sh.patch
 patch ./Ghidra/Framework/Docking/build.gradle ../patch/build.gradle.patch
 patch ./gradle/support/fetchDependencies.gradle ../patch/fetchDependencies.gradle.patch
 patch ./Ghidra/Framework/Docking/Module.manifest ../patch/Docking.Module.manifest
+
+#Processors Manuals
+wget 'https://raw.githubusercontent.com/bdarmofal/proc_manual/master/ghidra_manuals/Armv7AR_errata.pdf' -O ./Ghidra/Processors/ARM/data/manuals/Armv7AR_errata.pdf
+#todo take a look at https://github.com/rickhohler/ghidra_2020/blob/bfd123123eb886b65220571f5cbf60c64784d8d5/scripts/ghidra_processor_docs_downloader.py
 
 #grep -r 'import db.\*;' *|awk -F':' '{print "sed -i " " \"s|import db.*;|import db.*;import db.Record;|g\" "$1}'|bash
 
