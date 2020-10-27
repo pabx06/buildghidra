b30a9320956bae4df5ec8e06d32fe88d11b0ee98
johny english <pabx06@gmail.com>
2020-10-27 17:42:23 +0100

    more verbose build

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,15 +19,30 @@ echo "[+] Patching gradle script for FlatLaf"
 . ../scripts/patchFlatLaf.sh
 
 #Processors Manuals
-wget 'https://raw.githubusercontent.com/bdarmofal/proc_manual/master/ghidra_manuals/Armv7AR_errata.pdf' -O ./Ghidra/Processors/ARM/data/manuals/Armv7AR_errata.pdf
+echo "[+] Downloading ARM processor Manual"
+wget -q 'https://raw.githubusercontent.com/bdarmofal/proc_manual/master/ghidra_manuals/Armv7AR_errata.pdf' -O ./Ghidra/Processors/ARM/data/manuals/Armv7AR_errata.pdf
+wget -q 'https://docs.oracle.com/javase/specs/jvms/se8/jvms8.pdf' -O ./Ghidra/Processors/JVM/data/manuals/jvms8.pdf
 #todo take a look at https://github.com/rickhohler/ghidra_2020/blob/bfd123123eb886b65220571f5cbf60c64784d8d5/scripts/ghidra_processor_docs_downloader.py
 
-#grep -r 'import db.\*;' *|awk -F':' '{print "sed -i " " \"s|import db.*;|import db.*;import db.Record;|g\" "$1}'|bash
 
+
+echo "[+] Downloading Build Dependencies"
 gradle -q --init-script gradle/support/fetchDependencies.gradle init >/dev/null
+
+echo "[+] Copying Function ID Databases"
 cp -r ../ghidra-data/FunctionID/* Ghidra/Features/FunctionID/data
+
+echo "[+] Building Linux Natives"
 gradle -q buildNatives_linux64
+
+echo "[+] prepDev"
 gradle prepDev
+
+echo "[+] buildHelp"
 gradle buildHelp
+
+echo "[+] PeclipsePDE"
 gradle eclipse -PeclipsePDE
+
+echo "[+] buildGhidra"
 gradle -q -x ip buildGhidra
